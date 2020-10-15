9ba7a1c691a4f9464f186634c1388b40fcb3846f
Eirik Skjærseth <eirik1502@gmail.com>
2020-10-15 21:17:52 +0200

    include logger implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,9 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    runtimeOnly("org.slf4j:slf4j-simple:1.7.29")
     implementation("io.github.microutils:kotlin-logging:1.12.0")
-
 }
 
 tasks.withType<KotlinCompile> {
